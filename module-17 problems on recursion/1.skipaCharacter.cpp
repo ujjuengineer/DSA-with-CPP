395468fd8c39f9cdc71f280d15f6b05c4577c6c9
ujjuengineer <uk3945351@gmail.com>
2024-08-28 05:33:58 +0530

    added folder

---
+++ module-17 problems on recursion/1.skipaCharacter.cpp
@@ -0,0 +1,23 @@
+// remove all occurrece of 'a' from a string
+
+#include <iostream>
+using namespace std;
+
+void call(string str, string ans, int idx){
+    // base case
+    if(idx==str.size()){
+        cout<< ans;
+        return;
+    }
+    if(str[idx]!='a') ans+=str[idx]; 
+    call(str, ans, idx+1);
+}
+
+int main(){
+    string str;
+    cout<<"enter string : " ;
+    getline(cin, str);
+
+    string ans = "";
+    call(str, ans, 0);
+}
