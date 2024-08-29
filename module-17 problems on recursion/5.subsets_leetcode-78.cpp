d420428975aaed5d0a2c8a14997bd2c1e8849adb
ujjuengineer <uk3945351@gmail.com>
2024-08-29 06:00:16 +0530

    modified

--- module-17 problems on recursion/5.subsets_leetcode-78.cpp
+++ module-17 problems on recursion/5.subsets_leetcode-78.cpp
@@ -2,8 +2,19 @@
 // print subsets of a string with unique characters. 
 // do it for array as well
 // note that number of subsets = 2 to the power n where n = number of elements in sets
+
+
+/*
+traditional method to do so is, make 2 calls, in first call, upend the character of original string and in second call don't add the character. */
+
+
 #include<iostream>
 using namespace std;
+
+void print(string original, string ans){
+    
+}
+
 int main(){
-    cout<< "h";
+    
 }
