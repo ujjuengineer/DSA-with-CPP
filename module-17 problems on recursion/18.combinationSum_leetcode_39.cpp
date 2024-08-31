6c1eb946177d5edcc106f1db0e60b8097fd9740e
ujjuengineer <uk3945351@gmail.com>
2024-08-31 15:23:10 +0530

    added files

--- module-17 problems on recursion/18.combinationSum_leetcode_39.cpp
+++ module-17 problems on recursion/18.combinationSum_leetcode_39.cpp
@@ -18,16 +18,20 @@ void combination(vector<int> v, int arr[], int n, int target, int idx){
     // we have two option, either we can choose that element or we can skip that element, if we skiped that element it means, we have no use of that and we will move to the next index, otherwise we will remain with the same idx;
     // we use pop back so that at last when the sum of v exceeds, then we can
     v.push_back(arr[idx]);
-    // combination(v, arr, n, target-arr[idx], idx);
-    // v.pop_back();
-    combination(v, arr, n, target-arr[idx], idx+1);
+    combination(v, arr, n, target-arr[idx], idx);
     v.pop_back();
+    combination(v, arr, n, target, idx+1);
 }
 
 int main(){
     vector<int> v;
+    // all element of arr are distinct
     int arr[]={2,3,5};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    combination(v, arr, n, 8, 0);
+    int target;
+    cout<< "enter target: ";
+    cin>> target;
+
+    combination(v, arr, n, target, 0);
 }
