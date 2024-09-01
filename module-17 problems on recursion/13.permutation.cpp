b69d74df219dbeaa1eadaeea7879c243cf6d0783
ujjuengineer <uk3945351@gmail.com>
2024-09-02 05:23:02 +0530

    added files

--- module-17 problems on recursion/13.permutation.cpp
+++ module-17 problems on recursion/13.permutation.cpp
@@ -5,6 +5,11 @@
 -> we will make number of call = length of the string, as the length decreases, the number of call for recursion decreases. 
 -> at every call we will extract left and right side of the index and add them and send for the next call */
 
+
+// permutation me sare possible pattern print hoga, whereas combination me sirf unique combination hi print hoega
+// for example in permutation, 1,2,3 and 1,3,2 are 2 different species, whereas in combination, permutation 1,2,3 and 1,3,2 both are same
+
+
 #include<iostream>
 using namespace std;
 
